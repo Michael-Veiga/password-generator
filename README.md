9f0a4fd7b64e0818c019e0f6528bf58b0e649956
Mike V <mvee333@gmail.com>
2020-04-19 16:15:48 -0400

    created subarrays and changed rando num gen

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # password-generator
+
+# User Story 
+The purpose of this project was to create a random password generator that allows you to pick between 8 and 128 characters. It also allows you to select the type of charcaters you want in the password, including numbers, uppercase, lowercase and special. 
+
+# Acceptance Criteria
+The acceptance criteria for the generator is as follows;
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+# Fulfilling the Criteria
+I believe that I have met all required criteria for this homework assignment. 
+The password generator spits out a number that meets the parameters set out by the prompts, when first clicking the "Generate Password" Button. If the user does not click a certain prompt, then the page will not display the undesirable character type. 
+Additionally, javascript will actually re-prompt you to enter in a password length between the required parameters if you failed to do so.
+
+# Review
+This was pretty much the hardest homework assignment I have ever done in my life. I worked well over 40 hours on this assingment and just barely got it to function properly. The things I struggled with the most we're random number generation and the creation of sub-arrays. 
+![Password Generation Code](assets/images/NumberGen.JPG) (I dont think this link works, but theres an image in the assets folder)
+
+# Summary
+This project taught me the importance of pseudocoding and structured thinking. Without a roadmap to your destination, you're basically just driving around aimlessly. 
+
+
+
+
+
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,90 @@
+// Create an empty array for password
+var passwordArr = [];
+
+// Arrays of possible characters
+var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var upperArr = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+var lowerArr = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+var specialArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+// Determine Password Length
+function generatePassword() {
+
+    var passwordString = "";
+
+    var passLength = Number(prompt("How many characters do you want your password to be?")); {
+        console.log(passLength);
+        if (isNaN(passLength) || passLength < 8 || passLength > 128) {
+            passLength = Number(prompt("Password length must be between 8 - 128 characters."));
+
+        }
+        // User must choose what the password will contain
+        var wantNumbers = confirm("Do you want it to contain numbers?");
+        console.log(wantNumbers)
+        var wantUpperCase = confirm("Do you want uppercase characters?");
+        console.log(wantUpperCase)
+        var wantLowerCase = confirm("Do you want lowercase characters?");
+        console.log(wantLowerCase)
+        var wantSpecialCharacters = confirm("Do you want it to contain special characters?");
+        console.log(wantSpecialCharacters)
+    }
+    // If the user fails to pick a character type
+    while (!wantNumbers && !wantUpperCase && !wantLowerCase && !wantSpecialCharacters) {
+        alert("You must select one or more character types.");
+        wantNumbers = confirm("Do you want it to contain numbers?");
+        wantUpperCase = confirm("Do you want uppercase characters?");
+        wantLowerCase = confirm("Do you want lowercase characters?");
+        wantSpecialCharacters = confirm("Do you want it to contain special characters?");
+    }
+
+    // Take user responses and add them to password array
+    if (wantNumbers) {
+        passwordArr.push(numArr);
+
+    }
+
+    if (wantUpperCase) {
+        passwordArr.push(upperArr);
+
+    }
+
+    if (wantLowerCase) {
+        passwordArr.push(lowerArr);
+
+    }
+
+    if (wantSpecialCharacters) {
+        passwordArr.push(specialArr);
+    }
+
+    // For loop that will run through user choices
+    for (var i = 0; i < passLength; i++) {
+
+        let randomArrNum;
+        let chosenCharArr;
+        let randomCharNum;
+        let randomChar;
+
+        // Random Number generation
+        randomArrNum = parseInt(Math.floor(Math.random() * passwordArr.length));
+
+        chosenCharArr = passwordArr[randomArrNum];
+
+        randomCharNum = Math.floor(Math.random() * chosenCharArr.length);
+        console.log(chosenCharArr)
+
+        randomChar = chosenCharArr[randomCharNum];
+        console.log(randomChar);
+
+        passwordString += randomChar;
+    }
+
+    password = passwordString;
+    return passwordString;
+
+
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -10,110 +97,9 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
-
-// Create an empty array for password and text
-var passwordText = [];
-var passwordArr = [];
-
-// Arrays of possible characters
-var randomNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var randomUc = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
-var randomLc = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
-var randomSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-
-// Determine Password Length
-function generatePassword() {
-    var passLength = Number(prompt("How many characters do you want your password to be?"));
-    if (isNaN(passLength) || passLength < 8 || passLength > 128) {
-        passLength = Number(prompt("Password length must be between 8 - 128 characters."));
-        console.log(passLength);
-    }
-    // User must choose what the password will contain
-    var wantNumbers = confirm("Do you want it to contain numbers?");
-    console.log(wantNumbers)
-    var wantUpperCase = confirm("Do you want uppercase characters?");
-    console.log(wantUpperCase)
-    var wantLowerCase = confirm("Do you want lowercase characters?");
-    console.log(wantLowerCase)
-    var wantSpecialCharacters = confirm("Do you want it to contain special characters?");
-    console.log(wantSpecialCharacters)
-    // If the user fails to pick a character type
-    while (!wantNumbers && !wantUpperCase && !wantLowerCase && !wantSpecialCharacters) {
-        alert("You must select one or more character types.");
-        wantNumbers = confirm("Do you want it to contain numbers?");
-        wantUpperCase = confirm("Do you want uppercase characters?");
-        wantLowerCase = confirm("Do you want lowercase characters?");
-        wantSpecialCharacters = confirm("Do you want it to contain special characters?");
-    }
-}
-// Take user responses and add them to password array
-if (wantNumbers) {
-    passwordArr.push(randomNum);
-}
-
-if (wantUpperCase) {
-    passwordArr.push(randomUc);
-}
-
-if (wantLowerCase) {
-    passwordArr.push(randomLc);
-}
-
-if (wantSpecialCharacters) {
-    passwordArr.push(randomSpecial);
-}
-
-// For loop that will run through user choices
-for (var i = 0; i < 2; i++) {
-    passwordArr.push(getRandom(randomNum));
-}
-
-for (var i = 0; i < 2; i++) {
-    passwordArr.push(getRandom(randomUc));
-}
-
-for (var i = 0; i < 2; i++) {
-    passwordArr.push(getRandom(randomLc));
-}
-
-for (var i = 0; i < 2; i++) {
-    passwordArr.push(getRandom(randomSpecial));
-}
+generateBtn.addEventListener("click", writePassword);
 
 
-// Generator functions
-function getRandom(arr) {
-    var index = Math.floor(Math.random() * arr.length);
-    var char = arr[index];
-    console.log(index, char);
-    return char;
-}
-
-getRandom()
-
-
-
-// Checks to see if a variable is active, and assigns it an empty minimum
-// if (wantNumbers) {
-//     minNumbers = functionArray.getNumbers();
-//     minimumCount++;
-// }
-// if (wantUpperCase) {
-//     minUpperCase = functionArray.getUpperCase();
-//     minimumCount++;
-// }
-// if (wantLowerCase) {
-//     minLowerCase = functionArray.getLowerCase();
-//     minimumCount++;
-// }
-// if (wantSpecialCharacters) {
-//     minSpecialCharacters = functionArray.getSpecialCharacters();
-//     minimumCount++;
-// }
-
-// Loop used to get random characters
-// for (let i = 0, i < parseInt(passwordLength) - minimumCount); i++)
 
 
 
